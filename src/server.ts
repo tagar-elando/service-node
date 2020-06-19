610f428073097414013959ff58a98741bfe8106c
Allef Frazão Santana (Garug) <allefff@gmail.com>
2020-06-19 14:43:09 -0300

    Testando deploy

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import app from "./app";
+import cors from "cors";
+
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENT || "*",
+  optionsSuccessStatus: 200,
+};
+
+app.app.use(cors(corsOptions));
 
 app.server.listen(8080, function () {
   console.log(`App is running on port 8080!`);
