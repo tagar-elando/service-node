37f11750f1fc3ffae93933538d2797a60a779d64
William Barreto <bsi.william@gmail.com>
2020-06-24 22:37:10 -0300

    Refatorado estrutura do código

---
+++ src/interfaces.ts
@@ -0,0 +1,24 @@
+interface IIncomingMessage {
+    chatId: string;
+    message: string;
+  }
+  
+  interface IOutcomeMessage {
+    chatId: string;
+    user: string;
+    timestamp: Date;
+    message: string;
+  }
+  
+  interface IUserDisconnected {
+    chatId: string;
+    user: string;
+    timestamp: Date;
+  }
+  
+  interface IUserTyping {
+    chatId: string;
+    user: string;
+  }
+
+  export { IIncomingMessage, IOutcomeMessage, IUserDisconnected, IUserTyping};
